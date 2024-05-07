d406b560de43a4d7c6eb287250625d28da4eeeef
ameliepet <ameliepetersen2@gmail.com>
2024-05-07 15:38:00 +0200

    geändert

---
+++ beispiele/m2_5c_addform.php
@@ -0,0 +1,22 @@
+
+<!DOCTYPE html>
+<html lang="de">
+    <head>
+        <meta charset="UTF-8"/>
+        <title> Formular und addieren </title>
+    </head>
+    <body>
+        <label for="erstezahl">a</label>
+        <br>
+        <input type="text" id="zahla" name="a" size=30 placeholder="a">
+        <br>
+        <label for="zweitezahl">b</label>
+        <br>
+        <input type="text" id="zahlb" name="b" size=30 placeholder="b">
+        <br>
+    </body>
+</html>
+
+
+<?php
+    include 'm2_5a_standardparameter.php';
