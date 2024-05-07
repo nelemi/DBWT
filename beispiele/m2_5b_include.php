b22fceb65e8a0a657d2ee5878676d7050405e252
ameliepet <ameliepetersen2@gmail.com>
2024-05-07 14:15:47 +0200

    geändert

---
+++ beispiele/m2_5b_include.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Praktikum DBWT. Autoren:
+ * Amelie, Petersen, 3661323
+ * Nele, Mikkelsen, 3650167
+ */
+?>
+
+<?php
+    include 'm2_5a_standardparameter.php';
+
+    echo addieren(5, 5);
+    echo addieren(10);
+    echo addieren(30, 10);
