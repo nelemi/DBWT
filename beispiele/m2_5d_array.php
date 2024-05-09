97d77b2c7f32edfd49fa2763b1b9a27b838b6e2e
Nele Mikkelsen <nele.mikkelsen@gmail.com>
2024-05-09 13:41:05 +0200

    hey

--- beispiele/m2_5d_array.php
+++ beispiele/m2_5d_array.php
@@ -9,6 +9,31 @@ $famousMeals = [
     4 => ['name' => 'Jägerschnitzel mit Pommes',
         'winner' => 2019]
 ];
+function no_winners ($meals){
+    $no_winners = [];
+    for ($year = 2000; $year < 2025; $year++){
+        $no_winner = true;
+        foreach ($meals as $meal){
+            $winner = $meal['winner'];
+            if (is_array($winner)){
+                if (in_array($year, $winner)){
+                    $no_winner = false;
+                    break;
+                }
+                }else {
+                  if ($year == $winner){
+                    $no_winner = false;
+                    break;
+                }
+                  }
+            }
+        if ($no_winner){
+            $no_winners[] = $year;
+        }
+        }
+           return $no_winners;
+}
+$result = no_winners($famousMeals);
 ?>
 
 <!DOCTYPE html>
@@ -26,17 +51,19 @@ $famousMeals = [
             $winner = $arrays['winner'];
             $string_winner = "";
             if (is_array($winner) ) {
-                $string_winner = implode(',', $winner);
+                for ($i = count($winner)-1; $i >= 0; $i--) {
+                    $string_winner .= $winner[$i];
+                    if ($i > 0) {
+                        $string_winner .= ',';
+                    }
+                }
             }
             else {
                 $string_winner = strval( $winner );
             }
-            echo "<li>$name <br> $string_winner</li>";
-
-             //hallo
+            echo "<li>$name <br> $string_winner</li> <br>";
         } ?>
-
     </ol>
-
+<p> No winners for the years: <?php echo implode(",",$result) ?></p>
 </body>
 </html>
