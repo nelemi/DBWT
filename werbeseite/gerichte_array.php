f8995881d59d912d711e9038c1ff3156e3084edc
ameliepet <ameliepetersen2@gmail.com>
2024-05-15 16:31:12 +0200

    geändert

--- werbeseite/gerichte_array.php
+++ werbeseite/gerichte_array.php
@@ -1,7 +1,7 @@
 <?php
-;
-function gerichte ($gericht = 0) : string {
-    $gerichte_array = ["Rindfleisch mit Bambus, Kaiserschoten 
+
+function alle_gerichte () {
+    return ["Rindfleisch mit Bambus, Kaiserschoten 
                     und rotem Paprika, dazu Mie Nudeln",
         "Spinatrisotto mit kleinen Samasateigecken 
                     und gemischtem Salat",
@@ -10,42 +10,20 @@ function gerichte ($gericht = 0) : string {
                     karamellisierten Walnüssen",
         "Belgische Waffeln mit feiner Schokoladensauce, 
                     saisonalen Früchten und einer Kugel hausgemachtem 
-                    Vanilleeis"
-    ];
-        if (array_key_exists($gericht, $gerichte_array)) {
-            return $gerichte_array[$gericht];
+                    Vanilleeis"];
+}
+
+function gerichte ($gericht = 0) : string {
+    if (array_key_exists($gericht, alle_gerichte())) {
+            return alle_gerichte()[$gericht];
             } else {
-            return $gerichte_array[0];
+            return alle_gerichte()[0];
         }
 }
-$gerichte_array = ["Rindfleisch mit Bambus, Kaiserschoten 
-                    und rotem Paprika, dazu Mie Nudeln",
-    "Spinatrisotto mit kleinen Samasateigecken 
-                    und gemischtem Salat",
-    "Frische Tortelloni mit Ziegenkäse-Zitronen-
-                    Füllung auf grünem Spargel, verfeinert mit
-                    karamellisierten Walnüssen",
-    "Belgische Waffeln mit feiner Schokoladensauce, 
-                    saisonalen Früchten und einer Kugel hausgemachtem 
-                    Vanilleeis"
-];
 
-$anzahl_gerichte = count($gerichte_array);
+$anzahl_gerichte = count(alle_gerichte());
 
 
 
 
 
-
-
-
-//function gerichte ($gericht = 0) : string {
-    //$gerichte_array = [["Rindfleisch mit Bambus, Kaiserschoten
-          //          und rotem Paprika, dazu Mie Nudeln"],
-        //["Spinatrisotto mit kleinen Samasateigecken
-          //          und gemischtem Salat"]];
-    //if (isset($gerichte_array[$gericht])) {
-      //  $ger_array = $gerichte_array[$gericht];
-    //}return $ger_array;
-//}
-//echo gerichte();
