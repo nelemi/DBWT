790030a51c72674ebd61e7bf3d9b9720565de0d0
Nele Mikkelsen <nele.mikkelsen@gmail.com>
2024-05-20 12:41:32 +0200

    hey

--- werbeseite/werbeseite_daten.sql
+++ werbeseite/werbeseite_daten.sql
@@ -1,6 +1,37 @@
-
+CREATE DATABASE IF NOT EXISTS emensawerbeseite CHARACTER SET UTF8mb4 COLLATE utf8mb4_unicode_ci;
 USE emensawerbeseite;
 
+CREATE TABLE gericht (
+                         id  INT8 PRIMARY KEY,
+                         name VARCHAR(80) NOT NULL UNIQUE,
+                         beschreibung VARCHAR(800) NOT NULL,
+                         erfasst_am DATE NOT NULL,
+                         vegetarisch BOOLEAN NOT NULL,
+                         vegan BOOLEAN NOT NULL,
+                         preisintern DOUBLE NOT NULL CHECK (preisintern < preisextern),
+                         preisextern DOUBLE NOT NULL CHECK(preisextern > preisintern));
+
+CREATE TABLE allergen (
+                          code CHAR(4) PRIMARY KEY,
+                          name VARCHAR(300) NOT NULL,
+                          typ VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE kategorie (
+                           id INT8 PRIMARY KEY,
+                           name VARCHAR(80) NOT NULL,
+                           eltern_id INT8,
+                           bildname VARCHAR(200)
+);
+CREATE TABLE gericht_hat_allergen (
+                                      code CHAR(4),
+                                      gericht_id INT8 NOT NULL
+);
+
+CREATE TABLE gericht_hat_kategorie (
+                                       gericht_id INT8 NOT NULL,
+                                       kategorie_id INT8 NOT NULL
+);
 INSERT INTO `allergen` (`code`, `name`, `typ`) VALUES
 	('a', 'Getreideprodukte', 'Getreide (Gluten)'),
 	('a1', 'Weizen', 'Allergen'),
@@ -89,4 +120,25 @@ INSERT INTO `kategorie` (`id`, `eltern_id`, `name`, `bildname`) VALUES
 INSERT INTO `gericht_hat_kategorie` (`kategorie_id`, `gericht_id`) VALUES
 	(3, 1),	(3, 3),	(3, 4),	(3, 5),	(3, 6),	(3, 7),	(3, 9),	(4, 16), (4, 17), (4, 18), (5, 16), (5, 17), (5, 18);
 
+#Prüfen ob alle Daten eingelesen wurden pro Spalte
+SELECT COUNT(*) FROM allergen; # 21 entspricht Wahrheit
+SELECT COUNT(*) FROM gericht; #19 entspricht Wahrheit
+SELECT COUNT(*) FROM gericht_hat_allergen; #31 Wahrheit
+SELECT COUNT(*) FROM gericht_hat_kategorie; #13 passt
+SELECT COUNT(*) FROM kategorie; #7 passt auch
 
+#Alle Daten der Gerichte
+SELECT * FROM gericht;
+#Erfassungsdatum Gerichte
+SELECT erfasst_am FROM gericht;
+#Erfassungsdatum sowie den Namen (als Attributname Gerichtname) aller Gerichte absteigend sortiert nach Gerichtname
+SELECT erfasst_am, name AS Gerichtname FROM gericht ORDER BY Gerichtname DESC;
+#Namen und Beschreibung, aufsteigend...
+SELECT name, beschreibung FROM gericht ORDER BY name ASC LIMIT 10 OFFSET 5;
+SELECT DISTINCT typ FROM allergen;
+SELECT name FROM gericht WHERE name LIKE 'K%';
+SELECT id, name FROM gericht WHERE name LIKE '%suppe%';
+SELECT * FROM kategorie WHERE eltern_id IS NULL;
+UPDATE allergen SET name = 'Kamut' WHERE code = 'a6';
+INSERT INTO gericht (`id`, `name`, `beschreibung`, `erfasst_am`, `vegan`, `vegetarisch`, `preisintern`, `preisextern`) VALUES  (21, 'Currywurst mit Pommes', 'Leckerer Klassiker', '2024-05-19', 0, 0, 2.3, 3);
+INSERT INTO gericht_hat_kategorie (gericht_id, kategorie_id) VALUES (21, 3);
