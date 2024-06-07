4d173c2a544d81483d2ba2b6fea52d9fe93728dc
ameliepet <ameliepetersen2@gmail.com>
2024-06-07 16:28:05 +0200

    geändert

---
+++ emensa/config/db.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'host' => 'localhost',  // 'localhost' or '127.0.0.1'
+    'user' => 'root',       // '<yourusername>'
+    'password' => 'Swammy2504', // '<yourpassword>' webtech#12, Swammy2504
+    'database' => 'emensawerbeseite',
+    // optionally: set port below if it differs from the default 3306
+    // 'port' => 13306 // !! this is not your webserver port, but the mariadb port
+];
