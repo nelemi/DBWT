7d73d6ee4c88837812432135bde898d9a390092b
ameliepet <ameliepetersen2@gmail.com>
2024-06-10 20:53:43 +0200

    geändert

--- emensa/controllers/ExampleController.php
+++ emensa/controllers/ExampleController.php
@@ -4,15 +4,12 @@ require_once($_SERVER['DOCUMENT_ROOT'].'/../models/kategorie.php');
 class ExampleController
 {
     public function m4_6a_queryparameter(RequestData $rd) {
-        /*
-           Wenn Sie hier landen:
-           bearbeiten Sie diese Action,
-           so dass Sie die Aufgabe löst
-        */
+        $parametername = isset($GET['name']) ? $GET['name'] : '';
 
-        return view('notimplemented', [
+        return view('examples.m4_7a_queryparameter', [
             'request'=>$rd,
-            'url' => 'http' . (isset($_SERVER['HTTPS']) ? 's' : '') . '://' . "{$_SERVER['HTTP_HOST']}{$_SERVER['REQUEST_URI']}"
+            'url' => 'http' . (isset($_SERVER['HTTPS']) ? 's' : '') . '://' . "{$_SERVER['HTTP_HOST']}{$_SERVER['REQUEST_URI']}",
+            'name' => $parametername,
         ]);
     }
 }
---
+++ emensa/views/examples/m4_7a_queryparameter.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Query Parameter Beispiel</title>
+</head>
+<body>
+<p>Der Wert von name lautet: {{ $parametername }}</p>
+<!-- http://localhost:8080/m4_6a_queryparameter?name=Max zeigt alles an, außer den Namen
+-->
+</body>
+</html>
