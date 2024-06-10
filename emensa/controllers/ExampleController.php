50bdc95aba90f4e7fdfaa4809ae551061113983d
Nele Mikkelsen <nele.mikkelsen@gmail.com>
2024-06-10 22:18:54 +0200

    hey

--- emensa/controllers/ExampleController.php
+++ emensa/controllers/ExampleController.php
@@ -9,7 +9,21 @@ class ExampleController
         return view('examples.m4_7a_queryparameter', [
             'request'=>$rd,
             'url' => 'http' . (isset($_SERVER['HTTPS']) ? 's' : '') . '://' . "{$_SERVER['HTTP_HOST']}{$_SERVER['REQUEST_URI']}",
-            'name' => $parametername,
+            'names' => $parametername,
         ]);
+    }
+    public function m4_7b_kategorie(RequestData $rd) {
+        $link = connectdb();
+
+        $sql = 'SELECT DISTINCT ghk.kategorie_id, k.name
+                    FROM gericht_hat_kategorie ghk
+                    LEFT JOIN kategorie k ON ghk.kategorie_id= k.id
+                    ORDER BY name ASC';
+        $result = mysqli_query($link, $sql);
+
+        $data = mysqli_fetch_all($result, MYSQLI_BOTH);
+
+        mysqli_close($link);
+    }
     }
 }
--- emensa/views/examples/m4_7a_queryparameter.blade.php
+++ emensa/views/examples/m4_7a_queryparameter.blade.php
@@ -6,7 +6,7 @@
     <title>Query Parameter Beispiel</title>
 </head>
 <body>
-<p>Der Wert von name lautet: {{ $parametername }}</p>
+<p>Der Wert von name lautet: {{ $names }}</p>
 <!-- http://localhost:8080/m4_6a_queryparameter?name=Max zeigt alles an, außer den Namen
 -->
 </body>
