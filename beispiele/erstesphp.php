b2e6a071cc88d3a4558e0dd88de86c2ff38a0211
Nele Mikkelsen <nele.mikkelsen@gmail.com>
2024-06-10 14:29:06 +0200

    hey

--- beispiele/erstesphp.php
+++ beispiele/erstesphp.php
@@ -10,3 +10,22 @@
 echo "Erstes PHP-Skript <br>";
 phpinfo();
 
+?>
+
+<?php
+class Besucher {
+    private $name;
+    public function __construct() {
+        $this->name = "unbekannt";
+    }
+    public function getName(){#private Da man dass dann eh in getLabeledName ändert
+        return $this->name;
+    }
+    private function setName($name){
+        return $this->name = $name;
+    }
+    public function getLabeledName (){
+        echo "Name:". $this->name;
+    }
+}
+
