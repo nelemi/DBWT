0078ac79f6c1cef0b0c61194d6fb9d832c32b664
Nele Mikkelsen <nele.mikkelsen@gmail.com>
2024-06-12 12:44:47 +0200

    hey

--- emensa/controllers/WerbeseiteController.php
+++ emensa/controllers/WerbeseiteController.php
@@ -5,4 +5,13 @@ class WerbeseiteController
 { public function index(RequestData $request) {
     return view('hauptseite.pages.hauptseite_page', ['rd' => $request ]);
 }
+public function dbconnect() {
+    $gericht = db_gerichttabelle_select_all();
+    $allergen = db_allergen_select_all();
+    $gerichthatallergen = db_gericht_hat_allergen_select_all();
+    // Frage Daten aus kategorie ab:
+    // $data = db_kategorie_select_all();
+    return view('demo.dbdata', ['data' => $data]);
 }
+}
+// welche View dann?
