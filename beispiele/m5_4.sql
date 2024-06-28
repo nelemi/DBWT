4cb16df31815210b9a05042ac489451185e1e226
Nele Mikkelsen <nele.mikkelsen@gmail.com>
2024-06-28 22:25:34 +0200

    hey

--- beispiele/m5_4.sql
+++ beispiele/m5_4.sql
@@ -14,11 +14,11 @@ CREATE VIEW view_anmeldungen AS
 
 #c) alle vegetarischen Gerichte sowie die zugehörigen Kategorien zeigt.
 # Stellen Sie immer alle Kategorien dar, auch wenn dieser Kategorie kein Gericht zugeordnet ist.
-
+DROP VIEW IF EXISTS view_kategoriegerichte_vegetarisch;
 CREATE VIEW view_kategoriegerichte_vegetarisch AS
     SELECT g.id,g.name, k.name AS kategorie_name
     FROM gericht g
     RIGHT JOIN gericht_hat_kategorie ghk ON g.id = ghk.gericht_id
-    RIGHT JOIN kategorie k ON ghk.kategorie_id = k.id;
+    RIGHT JOIN kategorie k ON ghk.kategorie_id = k.id
     WHERE g.vegetarisch = 1;
 
