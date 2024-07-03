c6deb7fc2768d56527b580410665d27903e57f17
ameliepet <ameliepetersen2@gmail.com>
2024-07-03 23:51:04 +0200

    geändert

--- beispiele/m5_4.sql
+++ beispiele/m5_4.sql
@@ -14,11 +14,47 @@ CREATE VIEW view_anmeldungen AS
 
 #c) alle vegetarischen Gerichte sowie die zugehörigen Kategorien zeigt.
 # Stellen Sie immer alle Kategorien dar, auch wenn dieser Kategorie kein Gericht zugeordnet ist.
-DROP VIEW IF EXISTS view_kategoriegerichte_vegetarisch;
+#DROP VIEW IF EXISTS view_kategoriegerichte_vegetarisch;
+
 CREATE VIEW view_kategoriegerichte_vegetarisch AS
-    SELECT g.id,g.name, k.name AS kategorie_name
-    FROM gericht g
-    RIGHT JOIN gericht_hat_kategorie ghk ON g.id = ghk.gericht_id
-    RIGHT JOIN kategorie k ON ghk.kategorie_id = k.id
-    WHERE g.vegetarisch = 1;
+SELECT
+    k.name AS kategorie_name,
+    g.id AS gericht_id,
+    g.name AS gericht_name
+FROM
+    kategorie k
+        LEFT JOIN
+    gericht_hat_kategorie ghk ON k.id = ghk.kategorie_id
+        LEFT JOIN
+    gericht g ON ghk.gericht_id = g.id AND g.vegetarisch = 1
+WHERE
+    g.id IS NOT NULL # eigtl. unnötige Überprüfung, aber für Union müssen SELECT Statements gleich
+                    # aufgebaut sein, deshalb die WHERE Bedingung
+
+UNION # weil zwei verschiedene Abfragen benötigt werden: einmal alle Kategorien zu vegetarischen
+        # Gerichten und einmal alle Kategorien, zu denen es gar keine Gerichte gibt
+
+SELECT
+    k.name AS kategorie_name,
+    NULL AS gericht_id,
+    NULL AS gericht_name
+FROM
+    kategorie k
+        LEFT JOIN
+    gericht_hat_kategorie ghk ON k.id = ghk.kategorie_id
+        LEFT JOIN
+    gericht g ON ghk.gericht_id = g.id AND g.vegetarisch = 1
+WHERE
+    k.id NOT IN (
+        SELECT k.id # so werden auch all die Kategorien ausgegeben, die eben noch nicht in den
+                    # vegetarischen Gerichten vorgekommen sind (kategorie_id NOT IN Liste der vegetarischen gerichte von eben)
+        FROM
+            kategorie k
+                LEFT JOIN
+            gericht_hat_kategorie ghk ON k.id = ghk.kategorie_id
+                LEFT JOIN
+            gericht g ON ghk.gericht_id = g.id
+        WHERE
+            g.vegetarisch = 1
+    );
 
